a09daeef219accc724ccc4e8ae5e5b18d2f10b71
rassulmurat <rasusl1995@gmail.com>
2016-10-31 13:41:49 +0300

    iperf connected, new fucntion exec to execute proccess

--- checkLib/checklib.cpp
+++ checkLib/checklib.cpp
@@ -7,6 +7,33 @@ CheckLib::CheckLib()
 
 int CheckLib::check(QList<QString> *list)
 {
-    list->replace( 3, "OK");
+    QString tmp = list->value(2);
+    QString command = "iperf -c ";
+    command.append(tmp);
+    QString str = exec(command);
+    qDebug()<<str;
     return 0;
+}
+
+
+QString CheckLib::exec(QString cmd)
+{
+    char buffer[128];
+    QString result = "";
+    QByteArray barr = cmd.toLatin1();
+    const char *command = barr.data();
+    FILE* pipe = popen(command, "r");
+    if (!pipe) throw std::runtime_error("popen() failed!");
+    try {
+        while (!feof(pipe)) {
+            if (fgets(buffer, 128, pipe) != NULL)
+                result.append(buffer);
+                qDebug()<<buffer;
+        }
+    } catch (...) {
+        pclose(pipe);
+        throw;
+    }
+    pclose(pipe);
+    return result;
 }
--- checkLib/checklib.h
+++ checkLib/checklib.h
@@ -3,6 +3,12 @@
 
 #include "checklib_global.h"
 #include <QList>
+#include <iostream>
+#include <stdexcept>
+#include <stdio.h>
+#include <string>
+#include <QByteArray>
+#include <QDebug>
 
 class CHECKLIBSHARED_EXPORT CheckLib
 {
@@ -10,6 +16,9 @@ class CHECKLIBSHARED_EXPORT CheckLib
 public:
     CheckLib();
     int check(QList<QString> *list);
+
+private:
+    QString exec(QString);
 };
 
 #endif // CHECKLIB_H

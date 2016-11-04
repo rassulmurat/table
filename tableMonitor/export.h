66a83aa70c9291755fabe1604928abaca50a82b7
rassulmurat <rasusl1995@gmail.com>
2016-11-04 11:18:53 +0300

    Renamed files checker -> test

--- tableMonitor/export.h
+++ tableMonitor/export.h
@@ -20,20 +20,45 @@ public:
     Export();
     Export(QString *path);
     void setFilePath(QString *path);
+    //Set Table to export to file, QList[0] are headers
     void setTable(QList<QList<QString>> *list);
+
+    //Set Table to Export to file sets headers automatically
     void setTable(QList<QString> headers, QList<QList<QString>> data);
+
+    //Add QObbject to connect slots/signals to.
     void setSignals(QObject *obj);
+
+    /*
+     * Set Class for tableModel interaction.
+     * Automatically gets tableColumns and table data.
+     */
     void setTableModel(TableInteraction *table);
+
+    /*
+     * Function to clean table data stored in class.
+     * Used at the end of the writing.
+     */
     void cleanTableData();
 
-private:
-    void init();
+protected:
+    /*
+     * Main structure of the file.
+     * Change this if differenct type of structure needed.
+     */
     void structFile();
+
+    //Below are listed members of file structure
     void header();
     void createTable();
     void populateTable();
-    void insertRow(QList<QString>);
     void afterTable();
+
+private:
+    //Initialize pointer and documnt.
+    void init();
+
+    //Below is listed data stored in class.
     QTextCursor *cursor;
     QTextDocument *doc;
     QList<QList<QString>> tableData;
